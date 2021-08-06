917eeac6212bd1db2757e172289d8180836e0f3e
Waithira Njuguna <72735080+eunicenjuguna@users.noreply.github.com>
2021-08-06 17:47:16 +0300

    Update and rename @Njuguna.E.py to newton_Njuguna.E.py

---
+++ newton_Njuguna.E.py
@@ -0,0 +1,21 @@
+def task1():
+    print("Name:Eunice Njuguna")
+    print("Email address:eunice2915@gmail.com")
+    print("Twitterhandle:@Eunice1420165")
+    print("slack username:@Njuguna.E")
+    print("my Biostacks:genomics")
+task1()
+
+slack_username ="@Njuguna.E"
+twitter = "@Eunice1420165"
+def HammDist(slack_username,twitter):
+    a=slack_username
+    b=twitter
+    if len(a)>len(b):
+        print('Hamming Distance is:', len(a)-len(b))
+    elif len(b)>len(a):
+        print('Hamming Distance is:', len(b)-len(a))
+    else:
+        print('No Hamming Distance')
+        
+HammDist(slack_username,twitter)     
