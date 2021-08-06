f156c152b58d6e2ff21b99da9202010d32e9ee1e
Akinwale Akinjiola <akinjil@gmail.com>
2021-08-06 16:28:29 +0100

    Submission for stage 0 for Akinmode

---
+++ scripts_stage_0/newton_Akinmode.jl
@@ -0,0 +1,9 @@
+function hammstring(a,b)
+    return length(a) == length(b) ? sum([a[i] != b[i] for i in 1:length(a)]) : length(a) > length(b) ? sum([a[i] != b[i] for i in 1:length(b)]) : sum([a[i] != b[i] for i in 1:length(a)])
+end
+println("NAME: Akinwale")
+println("EMAIL: akinwaleakinjiola@gmail.com")
+println("SLACK: @Akinwale")
+println("BIOSTACK: DataScience")
+println("TWITTER: @Akinwale_shines")
+println(string("HAMMSTRING: ", hammstring("@Akinwale", "@Akinwale_shines")))
