be998c38fd38877080ce91514a37ab3cda6d58ee
Akinwale Akinjiola <akinjil@gmail.com>
2021-08-07 12:03:13 +0100

    Update newton_Modupe.py

--- scripts_stage_0/newton_Modupe.py
+++ scripts_stage_0/newton_Modupe.py
@@ -1,8 +1,9 @@
 
-name = "Modupe Ariyo"
-email = "ariyocedar@gmail.com"
-slackUsername = "@Modupe"
-twitter_handle = "@modeee"
+name = "NAME:Modupe Ariyo"
+email = "EMAIL:ariyocedar@gmail.com"
+slackUsername = "SLACK:@Modupe"
+biostack = "BIOSTACK:Genomics"
+twitter_handle = "TWITTER:@modeee"
 def hammingDistance(str1,str2):
      count = 0  #this is the incremented variable for loop
      ham = 0 #incremented variable for hammD
@@ -16,5 +17,4 @@ hamming_distance = hammingDistance(slackUsername, twitter_handle)
 
 
 
-print(name,email,slackUsername,twitter_handle, hamming_distance, sep="\n")
-56
+print(name,email,slackUsername,biostack,twitter_handle,hamming_distance, sep="\n")
