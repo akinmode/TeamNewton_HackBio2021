5729dc64d1f0259ba0a5defe1a99ad7c58ede9a6
Akinwale Akinjiola <akinjil@gmail.com>
2021-08-07 09:18:38 +0100

    Update bash_newton_stage0.sh

--- bash_newton_stage0.sh
+++ bash_newton_stage0.sh
@@ -12,21 +12,23 @@
 	NEWTON_FOLDER="TeamNewton_HackBio2021/scripts_stage_0"
 	OUTPUT_CSV="team_newton_stage_0.csv"
 echo "=============================================================="
-echo "CLONING TEAM NEWTON REPOSITORY @ GITHUB ..."
-	git clone $TEAM_REPO
+echo "CLONING TEAM NEWTON REPOSITORY @ "$TEAM_REPO
+	#git clone $TEAM_REPO
 echo " "
 echo "=============================================================="
 echo "CHECKING FOR REQUIRED DEPENDECIES ..."
-	REQ_DEPEND=("bash" "python3" "julia")
-echo "[INFO] The Following are required to run the submitted scripts: "${REQ_DEPEND[@]}
+	REQ_DEPEND=("bash" "python3" "julia" "Rscript")
+echo "[INFO] The Following packages are required to execute the submitted scripts: "${REQ_DEPEND[@]}
 	for i in "${REQ_DEPEND[@]}";do
 	    command -v $i >/dev/null 2>&1 || {
-	        echo >&2 "[CHECK]: $i package is required to run the submitted scripts";
-		echo "[CHECK]: Install '$i package'and rerun";
+	        echo >&2 "[WARNING] $i package is required to run the submitted scripts";
+			echo "[WARNING] Install $i package and rerun";
 	        exit 1;
 	    }
+		echo "[CHECK] $i package already installed!";
 	done
-echo "[INFO] All required packages are available on local machine"
+echo " "
+echo "[SUCCESS] All required packages are available on local machine"
 echo " "
 echo "==============================================================="
 echo "HACKBIO INTERNSHIP 2021: TEAM Newton"
@@ -38,14 +40,14 @@ _generate() {
 	#Loop through the designated folder to run the scripts beginning with 'newton'
 	for i in $NEWTON_FOLDER/newton_*;do
 	    case $i in
-		*".sh" ) bash $i | awk -F":" '{ORS=","}{print $2}' | sed 's/,$/\n/g' >> $OUTPUT_CSV; echo "[INFO] "$(basename -- $i) " extracted to "$OUTPUT_CSV;;
-      	*".py" ) python3 $i | awk -F":" '{ORS=","}{print $2}' | sed 's/,$/\n/g' >> $OUTPUT_CSV; echo "[INFO] "$(basename -- $i) " extracted to "$OUTPUT_CSV;;
-		*".jl" ) julia $i | awk -F":" '{ORS=","}{print $2}' | sed 's/,$/\n/g' >> $OUTPUT_CSV; echo "[INFO] "$(basename -- $i) " extracted to "$OUTPUT_CSV;;
-		*".R" ) Rscript $i | awk -F":" '{ORS=","}{print $2}' | sed 's/,$/\n/g' >> $OUTPUT_CSV; echo "[INFO] "$(basename -- $i) " extracted to "$OUTPUT_CSV;;
-   		esac
+		*".sh" ) bash $i | awk -F":" '{ORS=","}{print $2}' | sed 's/,$/\n/g' >> $OUTPUT_CSV && echo "[CHECK] "$(basename -- $i) " extracted to "$OUTPUT_CSV;;
+		*".py" ) python $i | awk -F":" '{ORS=","}{print $2}' | sed 's/,$/\n/g' >> $OUTPUT_CSV && echo "[CHECK] "$(basename -- $i) " extracted to "$OUTPUT_CSV;;
+		*".jl" ) julia $i | awk -F":" '{ORS=","}{print $2}' | sed 's/,$/\n/g' >> $OUTPUT_CSV && echo "[CHECK] "$(basename -- $i) " extracted to "$OUTPUT_CSV;;
+		*".R" ) Rscript $i | awk -F":" '{ORS=","}{print $2}' | sed 's/,$/\n/g' >> $OUTPUT_CSV && echo "[CHECK] "$(basename -- $i) " extracted to "$OUTPUT_CSV;;
+      	esac
 	done
 	echo " "
-	echo "[INFO] "$OUTPUT_CSV" generated!"
+	echo "[SUCCESS] "$OUTPUT_CSV" generated!"
 }
 _generate
 echo "[SUCCESS]TASK COMPLETED FOR TEAM NEWTON!"
