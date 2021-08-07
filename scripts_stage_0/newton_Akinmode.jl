065ee37aee8adbd12d2407713495916b31d25f7d
Akinwale Akinjiola <akinjil@gmail.com>
2021-08-07 12:14:41 +0100

    Update newton_Akinmode.jl

--- scripts_stage_0/newton_Akinmode.jl
+++ scripts_stage_0/newton_Akinmode.jl
@@ -1,7 +1,7 @@
 function hammstring(a,b)
     return length(a) == length(b) ? sum([a[i] != b[i] for i in 1:length(a)]) : length(a) > length(b) ? sum([a[i] != b[i] for i in 1:length(b)]) : sum([a[i] != b[i] for i in 1:length(a)])
 end
-println("NAME:Akinwale")
+println("NAME:Akinwale Akinjiola")
 println("EMAIL:akinjil@gmail.com")
 println("SLACK:@Akinwale")
 println("BIOSTACK:DataScience")
